a4d9c56927aa411b9e6b88d6c541a9359fb73dfd
hastirou <ihastiro@gmail.com>
2023-04-14 13:58:39 +0700

    fix router

--- src/modules/categories/router.ts
+++ src/modules/categories/router.ts
@@ -3,11 +3,11 @@ import * as controller from "./controllers/index.js";
 
 const router = Router();
 
-router.get("/categories", controller.readMany);
-router.get("/categories/:id", controller.read);
+router.get("/", controller.readMany);
+router.get("/:id", controller.read);
 // router.post("/", controller.invite);
-router.post("/categories/create", controller.create);
-router.patch("/categories/update/:id", controller.update);
-router.delete("/categories/delete/:id", controller.destroy);
+router.post("/", controller.create);
+router.patch("/:id", controller.update);
+router.delete("/:id", controller.destroy);
 
 export default router;
